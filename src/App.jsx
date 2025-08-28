cd7515b4fb04fc51a58f578e63b2a843ca70f98a
t3ni3 <karolinapetrovici9@gmail.com>
2025-08-28 20:06:57 +0300

    Sesiunea 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Homepage from "./pages/homepage";
+import Homepage from "./pages/Homepage/Homepage";
+import Navbar from "./components/Navbar/Navbar";
+import Add from "./pages/Add/Add";
 
 function App() {
   return (
     <>
-      <Homepage />
+    <Navbar />
+      <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/add" element={<Add />} />
+      </Routes>
     </>
   );
 }
