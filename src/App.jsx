98aece05fb0ff176fe404c949838dded3db65ab2
bogdanc99 <bogdandev96@gmail.com>
2025-08-28 10:31:45 -0700

    Branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import Homepage from "./pages/homepage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/boostrap.min.css';
 
 function App() {
   return (
     <>
+  
+      <Navbar />
       <Homepage />
+      <Footer />
+      
+
+    
     </>
   );
 }
+
+
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Navbar.css";
+import { Navbar, Nav, Container }
 
 const Navbar = () => {
-  return <div>Navbar</div>;
-};
+  return (
+   
+  );}
+
+  );
+
 
 export default Navbar;
