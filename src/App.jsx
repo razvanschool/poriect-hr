385c42cb6aa97cbdf824b4f40d11da25e0b96243
galatanuandrei <galatanu24@gmail.com>
2025-08-28 20:15:08 +0300

    prima incercare

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Homepage from "./pages/homepage";
+import NavbarConst from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
 
 function App() {
   return (
     <>
+      <NavbarConst />
       <Homepage />
+      <footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+
+
+
+const NavbarConst = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary navbar-top-center">
+      <Container>
+        <Navbar.Brand href="#home">HR</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto"> {/* m-auto = margin auto, centrează */}
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
-export default Navbar;
+export default NavbarConst;
+
+
+
