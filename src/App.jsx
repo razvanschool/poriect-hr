441c9bc7ba39c4ca160ae0672a551c3fc643e5ed
Razvan <razvan.palie@itschool.ro>
2025-08-30 11:43:42 +0200

    test fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./pages/Homepage/Homepage";
+import EmployeeList from "./pages/Angajati/Angajati";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<EmployeeList />} />
       </Routes>
       <Footer />
     </>
--- src/pages/Angajati/Angajati.jsx
+++ src/pages/Angajati/Angajati.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import "./EmployeeList.css";
+import React, { useEffect, useState } from "react";
 
 const EmployeeList = () => {
-  const [employees] = useState([
+  const [employees, setEmployees] = useState([
     { name: "Ion Popescu", role: "Developer", department: "IT" },
     {
       name: "Maria Ionescu",
@@ -11,6 +10,23 @@ const EmployeeList = () => {
     },
     { name: "George Vasilescu", role: "Manager", department: "Sales" },
   ]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/employee`)
+      .then((response) => response.json())
+      .then((data) => {
+        const ang = data.map((x) => {
+          return {
+            name: x.first_name + " " + x.last_name,
+            role: x.position,
+            department: x.department,
+          };
+        });
+        setEmployees([...employees, ...ang]);
+      })
+
+      .catch((err) => console.error("FAILEDDDD TO GET THE MOVIE!!", err));
+  }, []);
 
   return (
     <div className="employee-list">
